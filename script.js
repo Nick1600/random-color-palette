bfde422b14c4e22f45cd124e20eabd2837481431
nick1600 <77914421+Nick1600@users.noreply.github.com>
2021-07-13 12:19:51 +1200

    rgb value display working unpositioned

--- script.js
+++ script.js
@@ -11,6 +11,13 @@ const circFive = document.querySelector('.five');
 
 const circleList = [circOne, circTwo, circThree, circFour, circFive];
 
+//colour displays selectors
+const disOne = document.querySelector('.rgb-one');
+const disTwo = document.querySelector('.rgb-two');
+const disThree = document.querySelector('.rgb-three');
+const disFour = document.querySelector('.rgb-four');
+const disFive = document.querySelector('.rgb-five');
+
 //random RGB selector
 let randValue = () => {
     return Math.floor(Math.random() * 255);
@@ -64,3 +71,38 @@ circTwo.addEventListener('click', changeEach2);
 circThree.addEventListener('click', changeEach3);
 circFour.addEventListener('click', changeEach4);
 circFive.addEventListener('click', changeEach5);
+
+//display rgb values
+const displayRgbOne = () => {
+    const circOneStyles = getComputedStyle(circOne)
+    const circOneColor = circOneStyles.backgroundColor;
+    disOne.innerHTML = circOneColor;
+}
+const displayRgbTwo = () => {
+    const circTwoStyles = getComputedStyle(circTwo)
+    const circTwoColor = circTwoStyles.backgroundColor;
+    disTwo.innerHTML = circTwoColor;
+}
+const displayRgbThree = () => {
+    const circThreeStyles = getComputedStyle(circThree)
+    const circThreeColor = circThreeStyles.backgroundColor;
+    disThree.innerHTML = circThreeColor;
+}
+const displayRgbFour = () => {
+    const circFourStyles = getComputedStyle(circFour)
+    const circFourColor = circFourStyles.backgroundColor;
+    disFour.innerHTML = circFourColor;
+}
+const displayRgbFive = () => {
+    const circFiveStyles = getComputedStyle(circFive)
+    const circFiveColor = circFiveStyles.backgroundColor;
+    disFive.innerHTML = circFiveColor;
+    console.log(circFiveColor);
+}
+
+//color display event listeners
+button.addEventListener('click', displayRgbOne);
+button.addEventListener('click', displayRgbTwo);
+button.addEventListener('click', displayRgbThree);
+button.addEventListener('click', displayRgbFour);
+button.addEventListener('click', displayRgbFive);
